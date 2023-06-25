35dcfad8573be18249d67158f12c082c9ca5c7cf
J-Sabejon24 <sabejon9000@gmail.com>
2023-06-25 23:10:24 +0800

    first commit

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      <h2>Main Section</h2>
+      <p>Welcome to my website!</p>
+    </main>
+  );
+}
+
+export default Main;
